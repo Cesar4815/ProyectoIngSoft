3e0ede5b3f152682dfcdceaf847a1d172402f318
OctavioAGC <blood_octavio@hotmail.com>
2018-12-06 00:27:38 -0600

    avance video 8

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const Product = require('./models/product')
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -18,8 +21,21 @@ app.get('/api/product/:productId',(req, res) =>{
 })
 
 app.post('/api/product',(req, res) => {
- console.log(req.body)
- res.status(200).send({message:'el producto se ha recibido'})
+  console.log('POST /api/product')
+  console.log(req.body)
+
+  let product = new Product()
+  product.name = req.body.name
+  product.picture = req.body.picture
+  	product.price = req.body.price
+  	product.category =  req.body.category
+  	product.description = req.body.description
+  	
+  	product.save((err, productStored) => {
+  		if(err) res.status(500).send({message:`Error al salvar en la base de datos: ${err}`})
+
+  		res.status(200).send({product: productStored })
+  	})
 })
 
 app.put('/api/product/:productId', (req, res) =>{
@@ -30,6 +46,15 @@ app.delete('/api/product/:productId', (req, res)=>{
 
 })
 
-app.listen(port, () => {
+mongoose.connect('mongodb://localhost:27017/shop',(err, res) =>{
+  if(err) {
+  	return console.log(`Error al conectar a la base de datos: ${err}`)
+  }
+  	console.log('conexion a la base de datos establecida...')
+
+   app.listen(port, () => {
 	console.log(`API REST corriendo en http://localhost:${port}`)
+ })
+
 })
+
