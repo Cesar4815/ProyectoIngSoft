db84f300f4809367c67c16853227a04d01f044aa
OctavioAGC <blood_octavio@hotmail.com>
2018-12-10 21:38:28 -0600

    Proyecto terminado

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const hbs = require('express-handlebears')
 const app = express()
 const api = require('./routes')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.engine('.hbs',hbs({
+ 
+  defaultLayout: 'default',
+   extname:'.hbs'
+}))
+
 app.use('/api', api)
+app.get('/login',(req,res){ 
+ res.render('login')
+ })
 
 module.exports = app
