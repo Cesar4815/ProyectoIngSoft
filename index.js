8bbd68fa10e593fea5a0a6dd146d45f1f2167c4a
César <cesarelite129@gmail.com>
2018-12-11 19:20:58 -0600

    Handlebars y otros errores arreglados

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 const mongoose = require('mongoose')
 const app = require('./app')
-const config = require ('./config')
+const config = require('./config')
 
-mongoose.connect(config.db,(err, res) =>{
-  if(err) {
-  	return console.log(`Error al conectar a la base de datos: ${err}`)
+mongoose.connect(config.db, (err, res) => {
+  if (err) {
+    return console.log(`Error al conectar a la base de datos: ${err}`)
   }
-  	console.log('conexion a la base de datos establecida...')
+  console.log('Conexión a la base de datos establecida...')
 
   app.listen(config.port, () => {
-	console.log(`API REST corriendo en http://localhost:${config.port}`)
- })
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
+  })
 })
